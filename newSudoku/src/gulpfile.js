0c910450a075fddade658be0111678edd0d0fc42
shangxy <shangxy@zjrealtech.com>
2019-02-26 18:59:39 +0800

    样式完成

---
+++ newSudoku/src/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+
+gulp.task("webpack", () => {
+	const webpack = require("webpack-stream");
+	const config = require("./webpack.config.js");
+	gulp.src("./js/**/*.js")
+		.pipe(webpack(config))
+		.pipe(gulp.dest("../www/js"));
+});
+
+gulp.task("less", () => {
+	const less = require("gulp-less");
+	gulp.src("./less/*.less")
+		.pipe(less())
+		.pipe(gulp.dest("../www/css"))
+});
+
+gulp.task("default", ["webpack", "less"]);
+
+gulp.task("watch", () => {
+	gulp.watch("less/**/*.less", ["less"]);
+	gulp.watch("js/**/*.js", ["webpack"]);
+});
